0d86699a9df300b1a1216a988777a89989a0fe98
UltCheetah1 <gmzalkin@gmail.com>
2025-07-15 20:11:01 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+// Initialize Phaser
+const config = {
+  type: Phaser.AUTO,
+  width: 600,
+  height: 800,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+const game = new Phaser.Game(config);
+
+
+function preload() {
+  
+}
+
+function create() {
+
+}
+
+function update() {
+  
+}
+
+
+
